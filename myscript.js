c8252c047dd77e6fc8a8e615ed7008f742df9965
mimasu <takeknock@gmail.com>
2014-08-22 02:52:40 +0900

    1時間を超える場合の「分」の値修正

--- myscript.js
+++ myscript.js
@@ -1,7 +1,7 @@
-//自動でquality100%のもののうち、動画時間の一番長いものを自動的に再生する
+//自動でquality95%超のもののうち、動画時間の一番長いものを自動的に再生する
 
-var mid=[];
-var mtime=[];
+//var mid=[];
+//var mtime=[];
 var arry=[];
 const QUAL = 95;
 
@@ -10,22 +10,23 @@ function getMaxId(){
 		var q = $(this).html().match(/([0-9]*)%/);
 		var id = $(this).attr("id");
 		var f = $(this).html().match(/duration">\(([0-9]*) ?([a-z]+).*\)/);
-		//console.log("f:  "+f);
-		var time = $(this).html().match(/duration">\(([0-9]*).*([a-z]*).*([0-9]*).*([a-z]*).*\)/);
+		var time = $(this).html().match(/duration">\(([0-9]*) ?([a-z]*) ?([0-9]*) ?([a-z]*).*\)/);
 		
 		if(f[2]=="h"){
-			time[1] = parseInt(time[1]) + 60 * parseInt(f[1]);
+			//console.log("time: "+time);
+			//console.log("f: "+f[1]);
+			time[1] = parseInt(time[3]) + 60 * parseInt(f[1]);
+			//console.log("time:  "+time[1]);
 		}else{
 			time[1] = parseInt(time[1]);
 		}
 		
-		console.log("quality:" + q[1] + " id:" + id+" time"+time[1]);
+		console.log("quality:" + q[1] + " id:" + id+" time:"+time[1]);
 		//console.log(q[1]=="100");
 		if(parseInt(q[1]) > QUAL){
-			mid[mid.length] = id;
-			mtime[mtime.length] = time[1];
+			//mid[mid.length] = id;
+			//mtime[mtime.length] = time[1];
 			arry.push([id,time[1]]);
-			
 		}
 	});
 }
@@ -39,12 +40,12 @@ function jump(){
 	getMaxId();
 	//console.log(mid);
 	//console.log("#"+mid[0]);
-	console.log("sort前: "+arry);
+	//console.log("sort前: "+arry);
 	arry.sort(function(a,b){
 		return (a[1] > b[1])? -1 : 1;	
 	});
 	console.log("sort後: "+arry);
-	$("div#"+mid[0]).click();
+	//$("div#"+mid[0]).click();
 };
 
 jump();
